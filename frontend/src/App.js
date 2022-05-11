1d0c0e5b29ad86757abbc7c24eac77221d563d65
Henry Gann <hgan927@auckland.ac.nz>
2022-05-11 18:17:38 +1200

    Basic structure established. Added analog clock.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import AnalogClock from 'analog-clock-react';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  let options = {
+    width: "300px",
+    border: true,
+    borderColor: "#2e2e2e",
+    baseColor: "#17a2b8",
+    centerColor: "#459cff",
+    centerBorderColor: "#ffffff",
+    handColors: {
+      second: "#d81c7a",
+      minute: "#ffffff",
+      hour: "#ffffff"
+    }
+  };
+
+  const updateClock = () => {
+    let ausTime = new Date().toLocaleString("en-US", { timeZone: "Australia/Brisbane" });
+    let date = new Date(ausTime);
+
+    this.setState({
+      'options': {
+        ...this.state.options,
+        seconds: date.getSeconds(),
+        minutes: date.getMinutes(),
+        hours: date.getHours()
+      }
+    })
+  }
+
+  setInterval(updateClock, 1000);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+    <div className="row">
+      <div className="col card">
+        Todo
+      </div>
+      <div className="col card">
+        <AnalogClock {...options} />
+      </div>
     </div>
+    <div className="row">
+      <div className="col card">
+        Todo
+      </div>
+      <div className="col card">
+        Weather
+      </div>
+    </div>
+    </div>
+
   );
 }
 
