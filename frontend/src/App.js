18e5a88884a7ff42b3904304fa298912cfc08bb8
Henry Gann <hgan927@auckland.ac.nz>
2022-07-06 14:25:52 +1200

    Heavy graphical overhaul. Video background installed.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,61 +1,10 @@
-import './App.css';
-import { useState } from 'react';
-import AnalogClock from 'analog-clock-react';
+import LiveBackground from './Components/LiveBackground';
 
 function App() {
 
-  const [hour,setHour] = useState(0)
-  const [minute, setMinute] = useState(0)
-  const [options, setOptions] = useState({
-    width: "15vw",
-    border: true,
-    borderColor: "#2e2e2e",
-    baseColor: "#17a2b8",
-    centerColor: "#459cff",
-    centerBorderColor: "#ffffff",
-    handColors: {
-      second: "#ff0000",
-      minute: "#000000",
-      hour: "#000000"
-    }
-  })
-
-  const updateClock = () => {
-    let ausTime = new Date().toLocaleString("en-NZ");
-    let date = new Date(ausTime);
-    setMinute(date.getMinutes())
-    setHour(date.getHours())
-
-
-  setOptions({
-    ...options,
-    seconds: date.getSeconds(),
-    minutes: minute,
-    hours: hour,
-  })
-  }
-
-  setInterval(updateClock, 1000);
-
   return (
-    <div className="main">
-    <div className="row">
-      <div className="col card">
-        Todo
-      </div>
-      <div className="col card">
-        <AnalogClock {...options}/>
-        <p>{hour}:{minute}</p>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col card">
-        Todo
-      </div>
-      <div className="col card">
-        Weather
-      </div>
-    </div>
+    <div className = "home">
+      <LiveBackground/>
     </div>
 
   );
---
+++ frontend/src/Components/LiveBackground.js
@@ -0,0 +1,10 @@
+import Moon from '../Assets/full_moon.mp4';
+
+function LiveBackground () {
+    return(
+        <video className='video-background' src={Moon} autoPlay muted loop/>
+    );
+
+}
+
+export default LiveBackground;
