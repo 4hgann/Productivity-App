d46566ecb1f351b1120cfa7f61bd4c68b5bafd49
Henry Gann <hgan927@auckland.ac.nz>
2022-11-10 21:36:38 +1300

    Refactored countdown component to use a single button for start/stopping.

--- frontend/src/Components/Clock/Timer.js
+++ frontend/src/Components/Clock/Timer.js
@@ -6,8 +6,7 @@ import SetTimePane from './SetTimePane';
 
 const Timer = () => {
     const [[hours, minutes, seconds], setTime] = useState([0,0,0]);
-    const [startTimer, setTimer] = useState(false)
-    const [intervalID, setIntervalID] = useState([])
+    const [isRunning, setRunning] = useState(false)
 
     // For receiving input from SetTimePane
     const handleTimeInput = ([hrs, mins, secs]) => {
@@ -17,20 +16,20 @@ const Timer = () => {
     // Call the tick function every second or clear it if it is finished counting down.
     useEffect(() => {
         let interval;
-        if(startTimer){
+        if(isRunning){
             interval = setInterval( () => tick(), 1000);
         }
-        else if (!startTimer) {
+        else if (!isRunning) {
             clearInterval(interval)
         }
         return () => clearInterval(interval);
-    }, [startTimer, hours, minutes, seconds])
+    }, [isRunning, hours, minutes, seconds])
 
     // Handles the logic for changing time. Gets called every second to 'tick' down the timer
     const tick = () => {
         if(hours === 0 && minutes === 0 && seconds === 0){
             message.success('Time is up!')
-            setTimer(false)
+            setRunning(false)
         }
         else if (minutes === 0 && seconds === 0){
             setTime([hours - 1, 59, 59])
@@ -43,19 +42,21 @@ const Timer = () => {
         }
     }
 
-    const start = () => {
+    const handleClick = () => {
         if(hours === 0 && minutes === 0 && seconds === 0){
             message.error('You need to select a time')
         }
-        else{
-            message.success('Timer is now starting')
-            setTimer(true)
-        }
-    }
 
-    const stop = () => {
-            message.success('Timer is now pausing')
-            setTimer(false)
+        else{
+            setRunning(!isRunning)
+            if(!isRunning){
+                message.success('Starting timer')
+            }
+            else{
+                message.warn('Pausing timer')
+            }
+        }
+
     }
 
     return(
@@ -67,8 +68,7 @@ const Timer = () => {
                 `}
             </p>
             <div className='mainButtons'>
-                <Button className = 'timerButton' onClick={start}>Start</Button>
-                <Button className='timerButton' onClick={stop}>Pause</Button>
+                <Button className = 'timerButton' onClick={() => handleClick()}>{isRunning ? "Pause" : "Start"}</Button>
             </div>
             <div className="bottom">
                 <SetTimePane callbackHandler={handleTimeInput}/>
