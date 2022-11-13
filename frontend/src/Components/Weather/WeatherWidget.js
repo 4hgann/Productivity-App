8d312a21ee22c11b08344d06830eb9c0d71b1390
Henry Gann <hgan927@auckland.ac.nz>
2022-11-13 22:12:32 +1300

    Beginning setup for weather widget.

---
+++ frontend/src/Components/Weather/WeatherWidget.js
@@ -0,0 +1,17 @@
+import OpenWeatherAPIKey from "../../keys/OpenWeather";
+
+
+const WeatherWidget = () => {
+
+    return(
+        <div>
+            <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/weather-icons/2.0.9/css/weather-icons.min.css"
+            type="text/css"
+            />
+        </div>
+    )
+}
+
+export default WeatherWidget;
