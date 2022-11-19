11ca056f4b0baa650af54160474e763d3ed7977e
Henry Gann <hgan927@auckland.ac.nz>
2022-11-19 13:08:05 +1300

    Location autofill finished for weather widget settings. Uses Select component from antd which provides search functionality. Depends on a package containing methods for finding city data.

--- frontend/src/Components/Weather/SetLocationPane.js
+++ frontend/src/Components/Weather/SetLocationPane.js
@@ -2,22 +2,42 @@ import { useEffect, useRef, useState } from "react"
 import { Modal, Select } from "antd";
 import { IoMdSettings } from 'react-icons/io';
 import '../../Styles/WeatherWidget.css'
-import { Country } from "country-state-city";
+import { City, Country } from "country-state-city";
 
 
 function SetLocationPane({callbackHandler}) {
 
     const [showModal, toggleShowModal] = useState(false);
-    const [country,setCountry] = useState("")
+    const [cities, setCities] = useState([])
+    const [currentCity, setCurrentCity] = useState(null)
 
+    const countries = [].concat(...Country.getAllCountries()).map(({name, isoCode})=>{
+        return {value: name, label: name, countryCode: isoCode}
+    });
+    
+    let chosenCountry;
+    let chosenCity;
+    
     const toggle = () => toggleShowModal(!showModal)
+
     const submit = () =>{
-        console.log(country)
+        console.log(chosenCountry)
     }
 
-    const countries = [].concat(...Country.getAllCountries()).map(({name})=>{
-        return {value: name, label: name}
-    });
+    const countryChangeHandler = (value) => {
+        chosenCountry = value;
+        setCurrentCity(null)
+        // Fetch country code of element
+        const countryData = countries.find((country) => country.value === chosenCountry)
+        setCities( [].concat(...City.getCitiesOfCountry(countryData.countryCode)).map(({name}) => {
+            return {value: name, label:name}
+        }))
+    }
+    
+
+
+
+
 
     return(
         <>
@@ -31,9 +51,23 @@ function SetLocationPane({callbackHandler}) {
                 filterOption={(inputValue, option) =>
                 option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
                 }
+                value={chosenCountry}
+                onChange={(value) => countryChangeHandler(value)}
                 allowClear
-                value={country}
-                onChange={(value) => setCountry(value)}
+                showSearch
+                style={{width:"300px"}}
+            />
+
+            <Select
+                options={cities}
+                placeholder="Choose a city"
+                optionFilterProp="children"
+                filterOption={(inputValue, option) =>
+                option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+                }
+                value={currentCity}
+                allowClear
+                onChange={(value) => setCurrentCity(value)}
                 showSearch
                 style={{width:"300px"}}
             />
