faff0c2f63215c1fee71a43d4ae29fa30ce95845
Henry Gann <hgan927@auckland.ac.nz>
2022-12-19 20:41:53 +1300

    Overhauling previous todo styling and finished implementing todo functionality.

--- frontend/src/Components/Todos/TodoItem.js
+++ frontend/src/Components/Todos/TodoItem.js
@@ -1,15 +1,19 @@
+import { Button } from "antd";
 import { useContext } from "react";
+import { IconContext } from "react-icons";
 import { IoMdTrash, IoMdCheckmark } from "react-icons/io";
 import { todoContext } from "../../Contexts/TodoContext";
 import '../../Styles/TodoItem.css'
 export default function TodoItem({todo, index}){
-    const { deleteTodo } = useContext(todoContext)
+    const { deleteTodo, toggleComplete } = useContext(todoContext)
     return(
         <div className="todo-item">
-            <p className="left">{todo.name} due: {todo.due}</p>
+            <p className="left">{todo.name} - {todo.due} </p>
             <div className="buttons">
-                <button className="todo-button button"><IoMdCheckmark/></button>
-                <button className="todo-button button" onClick = {() => deleteTodo(index)}><IoMdTrash/></button>
+                <IconContext.Provider value={{size: '20px'}}>
+                    <button className ='todo-btn complete' onClick={() => toggleComplete(index)}><IoMdCheckmark className="icon"/></button>
+                    <button className ='todo-btn trash' onClick = {() => deleteTodo(index)}><IoMdTrash className="icon"/></button>
+                </IconContext.Provider>
             </div>
         </div>
     )
--- frontend/src/Contexts/TodoContext.js
+++ frontend/src/Contexts/TodoContext.js
@@ -5,42 +5,48 @@ export const todoContext = createContext({});
 export function TodoContextProvider({ children }){
 
     // Format of todos: {name: string, due: unix date-time number, isCompleted: boolean}
-    const [todos,setTodos] = useState([{name:'dick', due: '21/07', isCompleted: false}, {name:'dishes', due: '21/07', isCompleted: false}])
-    const [earliestFirst, setEarliestFirst] = useState(false)
+    const [todos,setTodos] = useState([{
+      "name": "test",
+      "due": "19/12/2022",
+      "unixTime": 1671431948296,
+      "isCompleted": false
+    }])
+    const [earliestFirst, setEarliestFirst] = useState(true)
     const [refresh, setRefresh] = useState(false)
 
     // Input validation and add the todo to array if valid else log 'error'
     const addTodo = (todo) => {
-      console.log('attempting to add todo: ', todo)
+      //console.log('attempting to add todo: ', todo)
       let newTodos = [...todos]
       newTodos.push(todo)
 
       setTodos(newTodos)
       setRefresh((value) => !value)
-
     }
 
     // Given an index, remove that todo from todos
     const deleteTodo = (index) => {
       let newTodos = [...todos]
       newTodos.splice(index,1)
+      
       setTodos(newTodos)
     }
 
     // Given an index of a todo, toggle the complete status
     const toggleComplete = (index) => {
-
+        let newTodos = [...todos]
+        newTodos[index].isCompleted = !newTodos[index].isCompleted
+        setTodos(newTodos)
     }
 
     useEffect(() => {
-        const unorderedTodos = [...todos]
-        // Order by date
-        //console.log(todos)
+        const newTodos = [...todos]
+        newTodos.sort((a, b) => a.unixTime - b.unixTime);
+        // Reverse the todos after sorting for reverse order
         if(!earliestFirst){
-            // Reverse the todos
+          newTodos.reverse()
         }
-
-        // setTodos(orderedTodos)
+        setTodos(newTodos)
     }, [refresh])
   
 
