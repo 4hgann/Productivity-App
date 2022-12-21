94bd1731f5e6339a265b296338717287e6c43f2c
Henry Gann <hgan927@auckland.ac.nz>
2022-12-21 22:20:58 +1300

    Improved todo item design. Looks better with new colour on completion.

--- frontend/src/Components/Todos/TodoItem.js
+++ frontend/src/Components/Todos/TodoItem.js
@@ -1,7 +1,6 @@
-import { Button } from "antd";
 import { useContext } from "react";
 import { IconContext } from "react-icons";
-import { IoMdTrash, IoMdCheckmark } from "react-icons/io";
+import { IoMdTrash, IoMdCheckmark, IoMdClose } from "react-icons/io";
 import { todoContext } from "../../Contexts/TodoContext";
 import '../../Styles/TodoItem.css'
 export default function TodoItem({todo, index}){
@@ -11,7 +10,10 @@ export default function TodoItem({todo, index}){
             <p className="left">{todo.name} - {todo.due} </p>
             <div className="right">
                 <IconContext.Provider value={{size: '20px'}}>
-                    <button className ='todo-btn complete-btn' onClick={() => toggleComplete(index)}><IoMdCheckmark className="icon"/></button>
+                    {todo.isCompleted ? 
+                    <button className ='todo-btn incomplete-btn' onClick={() => toggleComplete(index)}><IoMdClose className="icon"/></button> :
+                    <button className ='todo-btn complete-btn' onClick={() => toggleComplete(index)}><IoMdCheckmark className="icon"/></button> }
+                    
                     <button className ='todo-btn trash-btn' onClick = {() => deleteTodo(index)}><IoMdTrash className="icon"/></button>
                 </IconContext.Provider>
             </div>
